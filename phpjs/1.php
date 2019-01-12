b422cdd5e5909910a6efecaae919ecde21f5668b
Jungea <jungea2241@gmail.com>
2019-01-12 12:54:39 +0900

    php/js파트 수업 실습코드-1

---
+++ phpjs/1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>php</h1>
+	<?php
+	echo 10+10;
+	?>
+	<h1>JavaScript</h1>
+	<script>
+		document.write("hello world")
+	</script>
+</body>
+</html>
---
+++ phpjs/4.php
@@ -0,0 +1,21 @@
+<?php 
+$name = "Jung"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>javascript</h1>
+	<script>
+		name = "Jung";
+		document.write("안녕하세요."+name);
+	</script>
+	<h1>php</h1>
+	<?php 
+	echo "안녕하세요.".$name;
+	?>
+</body>
+</html>
